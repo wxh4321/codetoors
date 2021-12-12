8b241bc1c61a9a5ded20c7d3172c5f4c4d6420f0
wxh <hongtianyi@outlook.com>
2021-12-12 22:20:28 +0800

    fix

--- twofunc/test.js
+++ twofunc/test.js
@@ -1,21 +1,31 @@
 const  environment = [
-    ['A','X','R','R','R'],
-    ['O','O','O','X','B'],
-    ['O','O','O','O','R']
+    ["A","X","R","R","R"],
+    ["O","O","O","X","R"],
+    ["O","O","O","B","R"]
 ]
+
+// const  environment = [['A','X','R','R','R'],['O','O','O','X','B'],['O','O','O','O','R']]
 
 function rewardShortPaths(env){
     let res = [];
     let i = 0; // up or down 
     let j = 0; // only right
-   
+    let visited = [];
     function getminpath(env,i,j,arr,direction='right',pathcount=0,rewardnum=0){
+        if(!visited.includes(i+','+j)){
+            visited.push(i+','+j);
+        }
+        
         if(env[i]==undefined){
             return;
         }
         if(env[i][j]==undefined){
-            getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
-            getminpath(env,i+1,j-1,arr,'down',pathcount,rewardnum);
+            if(i-1>=0&&!visited.includes((i-1)+','+(j-1))){
+                getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
+            }
+            if(i+1<=env.length-1&&!visited.includes((i+1)+','+(j-1))){
+                getminpath(env,i+1,j-1,arr,'down',pathcount,rewardnum);
+            }
             return;
         }
         if(env[i][j]=='B'){
@@ -26,21 +36,33 @@ function rewardShortPaths(env){
             return;
         }
         if(env[i][j]=='O'||env[i][j]=='R'){
+            // console.log(visited)
+            // console.log(i+','+j);
             pathcount+=1;
         }
         if(env[i][j]=='R'){
             rewardnum+=1;
         }
         if(env[i][j]=='X'&&direction=='right'){
-            getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
-            getminpath(env,i+1,j-1,arr,'down',pathcount,rewardnum);
+            if(i-1>=0){
+                getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
+            }
+            if(i+1<=env.length-1){
+                getminpath(env,i+1,j-1,arr,'down',pathcount,rewardnum);
+            }
+           
         }
         else if(env[i][j]=='X'&&direction=='up'){
             getminpath(env,i-1,j+1,arr,'right',pathcount,rewardnum);
-            getminpath(env,i,j,arr,'down',pathcount,rewardnum);
+            if(i<=env.length-1){
+                getminpath(env,i,j,arr,'down',pathcount,rewardnum);
+            }
+           
         }
         else if(env[i][j]=='X'&&direction=='down'){
-            getminpath(env,i,j,arr,'up',pathcount,rewardnum);
+            if(i>=0){
+                getminpath(env,i,j,arr,'up',pathcount,rewardnum);
+            }
             getminpath(env,i+1,j+1,arr,'right',pathcount,rewardnum);
         }
         else{
