c0f38e84b83fd9848e13a77cb89523eb7e381a43
wangxinhong <wangxinhong@jzsec.com>
2021-12-13 10:44:46 +0800

    add

--- twofunc/python/twofunc.py
+++ twofunc/python/twofunc.py
@@ -11,54 +11,54 @@ def rewardShortPaths(env):
     i = 0 # up or down
     j = 0 # only right
     visited = []
-    def getminpath(env,i,j,arr,direction='right',pathcount=0,rewardnum=0):
-        if str(i)+','+str(j) not in visited:
+    def getminpath(env,i,j,arr,direction='right',pathcount=0,rewardnum=0): # find next point could pass, if not ,go back
+        if str(i)+','+str(j) not in visited: #record visited point
             visited.append(str(i)+','+str(j))
 
-        if i >= len(env) or i < 0:
+        if i >= len(env) or i < 0: # row out of range
             return
-        if j >= len(env[0]) or j < 0:
+        if j >= len(env[0]) or j < 0: # col at right out of range
             if i - 1 >= 0 and str(i-1)+','+str(j-1) not in visited:
                 getminpath(env, i - 1, j - 1, arr, 'up', pathcount, rewardnum)
             if i+1 <= len(env)-1 and str(i+1)+','+str(j-1) not in visited:
                 getminpath(env, i + 1, j - 1, arr, 'down', pathcount, rewardnum)
             return
 
-        if env[i][j] == 'B':
+        if env[i][j] == 'B': # point value is 'B' and record reward number and path steps number
             pathcount = pathcount+1
             arr.append(rewardnum)
             arr.append(pathcount)
             res.append([arr.pop(), arr.pop()])
             return
 
-        if env[i][j] == 'O' or env[i][j] == 'R':
+        if env[i][j] == 'O' or env[i][j] == 'R': # count path step number
             pathcount = pathcount + 1
 
-        if env[i][j]=='R':
+        if env[i][j]=='R': # count reward number 
             rewardnum = rewardnum + 1
-        if env[i][j] == 'X' and direction == 'right':
+        if env[i][j] == 'X' and direction == 'right': # point value is 'X' and moving direction is 'right' go back a step and point moving right go up or down
             if i-1>=0 :
                 getminpath(env, i - 1, j - 1, arr, 'up', pathcount, rewardnum)
             if i+1<=len(env)-1:
                 getminpath(env, i + 1, j - 1, arr, 'down', pathcount, rewardnum)
 
-        elif env[i][j] == 'X' and direction == 'up':
+        elif env[i][j] == 'X' and direction == 'up': # point value is 'X' and moving direction is 'up' go back a step and point moving right go right or down
             getminpath(env, i - 1, j + 1, arr, 'right', pathcount, rewardnum)
             if i <= len(env) - 1:
                 getminpath(env, i, j, arr, 'down', pathcount, rewardnum)
-        elif env[i][j]=='X'and direction=='down':
+        elif env[i][j]=='X'and direction=='down': # point value is 'X' and moving direction is 'down' go back a step and point moving right go right or up
             if i >= 0:
                 getminpath(env, i, j, arr, 'up', pathcount, rewardnum)
             getminpath(env, i + 1, j + 1, arr, 'right', pathcount, rewardnum)
-        else:
+        else: # defalut go right always
             getminpath(env, i, j + 1, arr, 'right', pathcount, rewardnum)
 
 
-    def findAllPaths(env):
+    def findAllPaths(env): # find all paths like [7,3] save in a array as [[7,3],[7,1]]
         arr = []
         getminpath(env, i, j + 1, arr)
 
-    def analysisPaths(arr):
+    def analysisPaths(arr): # analysis paths and find a correct result like [7,3]
         min = float("inf")
         minIndex = 0
         for i in range(0,len(arr)):
@@ -73,7 +73,7 @@ def rewardShortPaths(env):
         return (arr[minIndex][0],arr[minIndex][1])
 
     findAllPaths(env)
-    return analysisPaths(res)
+    return analysisPaths(res) # return [7,3]
 
 res = rewardShortPaths(environment)
 print(res)
--- twofunc/test.js
+++ twofunc/test.js
@@ -11,15 +11,15 @@ function rewardShortPaths(env){
     let i = 0; // up or down 
     let j = 0; // only right
     let visited = [];
-    function getminpath(env,i,j,arr,direction='right',pathcount=0,rewardnum=0){
-        if(!visited.includes(i+','+j)){
+    function getminpath(env,i,j,arr,direction='right',pathcount=0,rewardnum=0){ // find next point could pass, if not ,go back
+        if(!visited.includes(i+','+j)){ // record visited point
             visited.push(i+','+j);
         }
         
-        if(env[i]==undefined){
+        if(env[i]==undefined){ // row out of range
             return;
         }
-        if(env[i][j]==undefined){
+        if(env[i][j]==undefined){ // col at right out of range
             if(i-1>=0&&!visited.includes((i-1)+','+(j-1))){
                 getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
             }
@@ -28,22 +28,22 @@ function rewardShortPaths(env){
             }
             return;
         }
-        if(env[i][j]=='B'){
+        if(env[i][j]=='B'){ // point value is 'B' and record reward number and path steps number
             pathcount+=1;
             arr[arr.length] = rewardnum;
             arr[arr.length] = pathcount;
             res.push([arr.pop(),arr.pop()]);
             return;
         }
-        if(env[i][j]=='O'||env[i][j]=='R'){
+        if(env[i][j]=='O'||env[i][j]=='R'){ // count path step number
             // console.log(visited)
             // console.log(i+','+j);
             pathcount+=1;
         }
-        if(env[i][j]=='R'){
+        if(env[i][j]=='R'){ // count reward number 
             rewardnum+=1;
         }
-        if(env[i][j]=='X'&&direction=='right'){
+        if(env[i][j]=='X'&&direction=='right'){ // point value is 'X' and moving direction is 'right' go back a step and point moving right go up or down
             if(i-1>=0){
                 getminpath(env,i-1,j-1,arr,'up',pathcount,rewardnum);
             }
@@ -52,30 +52,30 @@ function rewardShortPaths(env){
             }
            
         }
-        else if(env[i][j]=='X'&&direction=='up'){
+        else if(env[i][j]=='X'&&direction=='up'){ // point value is 'X' and moving direction is 'up' go back a step and point moving right go right or down
             getminpath(env,i-1,j+1,arr,'right',pathcount,rewardnum);
             if(i<=env.length-1){
                 getminpath(env,i,j,arr,'down',pathcount,rewardnum);
             }
            
         }
-        else if(env[i][j]=='X'&&direction=='down'){
+        else if(env[i][j]=='X'&&direction=='down'){ // point value is 'X' and moving direction is 'down' go back a step and point moving right go right or up
             if(i>=0){
                 getminpath(env,i,j,arr,'up',pathcount,rewardnum);
             }
             getminpath(env,i+1,j+1,arr,'right',pathcount,rewardnum);
         }
-        else{
+        else{ // defalut go right always
             getminpath(env,i,j+1,arr,'right',pathcount,rewardnum);
         }
         
     }
-    function findAllPaths(env){
+    function findAllPaths(env){ // find all paths like [7,3] save in a array as [[7,3],[7,1]]
         // order up right down
         const arr = []; 
         getminpath(env,i,j+1,arr);
     }
-    function analysisPaths(arr){
+    function analysisPaths(arr){ // analysis paths and find a correct result like [7,3]
         let min = Infinity;
         let minIndex = 0;
         arr.forEach((item,i)=>{
@@ -93,7 +93,7 @@ function rewardShortPaths(env){
         return arr[minIndex];
     }
     findAllPaths(env);
-    return analysisPaths(res);
+    return analysisPaths(res); // return [7,3]
 }
 
 const res = rewardShortPaths(environment);
